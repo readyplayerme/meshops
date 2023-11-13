438e5f64c9cce818fa5285b75cffcf28885ad406
TechyDaniel <117300186+TechyDaniel@users.noreply.github.com>
2023-11-13 23:49:46 +0100

    feat: add get_edges_from_mesh to extract edges from mesh
    - Add tests for get_edges_from_mesh and border vertex extraction.
    - Introduce mock_data.py for consistent mock usage.
    - Refactor get_border_vertices to take an array instead of a mesh.

--- src/readyplayerme/meshops/mesh.py
+++ src/readyplayerme/meshops/mesh.py
@@ -38,13 +38,31 @@ def read_glb(filename: str | Path) -> Mesh:
     return trimesh.load(filename, process=False, force="mesh")
 
 
-def get_border_vertices(mesh: Mesh) -> npt.NDArray[np.int64]:
+def get_edges_from_mesh(mesh: Mesh) -> npt.NDArray[np.int32]:
+    """Return the unsorted edges of the mesh.
+
+    :param mesh: The mesh to get the edges from.
+    :return: The unsorted edges of the mesh.
+    :raises TypeError: If mesh is not a supported mesh type.
+    """
+    # Check if it's a Trimesh mesh
+    if isinstance(mesh, trimesh.Trimesh):
+        return mesh.edges
+
+    # TODO: Add additional mesh type checks and return their edges
+    """ elif isinstance(mesh, SomeOtherMeshType):
+            return mesh.some_other_method_to_get_edges()"""
+    msg = f"Unsupported mesh format: {type(mesh)}"
+    raise NotImplementedError(msg)
+
+
+def get_border_vertices(edges: npt.NDArray[np.int32]) -> npt.NDArray[np.int32]:
     """Return the indices of the vertices on the borders of a mesh.
 
-    :param mesh: The mesh to get the border vertices from.
+    :param edges: The unsorted edges of the mesh.
     :return: The indices of the border vertices.
     """
-    edges = mesh.edges_sorted
-    unique_edges, edge_triangle_count = np.unique(edges, return_counts=True, axis=0)
+    sorted_edges = np.sort(edges, axis=1)
+    unique_edges, edge_triangle_count = np.unique(sorted_edges, return_counts=True, axis=0)
     border_edge_indices = np.where(edge_triangle_count == 1)[0]
     return np.unique(unique_edges[border_edge_indices])
--- tests/readyplayerme/meshops/test_mesh.py
+++ tests/readyplayerme/meshops/test_mesh.py
@@ -1,9 +1,15 @@
 """Unit tests for the mesh module."""
+import sys
 from pathlib import Path
+
+import numpy as np
 
 from readyplayerme.meshops import mesh
 
 mocks_path = Path(__file__).parent.parent.parent / "mocks"
+
+# Add the project root to the Python path to import mock_data.py
+sys.path.insert(0, str(Path(__file__).resolve().parents[3]))
 
 
 def test_read_glb():
@@ -13,13 +19,36 @@ def test_read_glb():
     assert isinstance(result, mesh.Mesh), "The result should be an instance of mesh.Mesh"
 
 
+def test_edges_from_mesh_trimesh():
+    """Test the get_edges_from_mesh function returns expected edges."""
+    from tests.mocks import mock_data
+
+    expected_edges = mock_data.EDGES
+    edges = mesh.get_edges_from_mesh(mock_data.mock_mesh_trimesh)
+
+    assert np.array_equal(
+        edges, expected_edges
+    ), "The edges returned by get_edges_from_mesh do not match the expected edges."
+
+
 def test_border_vertices():
     """Test the get_border_vertices function returns the expected indices."""
-    import numpy as np
+    from tests.mocks import mock_data
 
-    # TODO: create a fixture for loading the file, or mock it, loading the mesh should not be part of the test!
+    edges = mock_data.EDGES
+    border_vertices = mesh.get_border_vertices(edges)
+
+    assert np.array_equiv(
+        np.sort(border_vertices), [0, 2, 4, 6, 7, 9, 10]
+    ), "The vertices returned by get_border_vertices do not match the expected vertices."
+
+
+def test_glb_border_vertices_flow():
+    """Test the flow of extracting border vertices from a glb file."""
     filename = mocks_path / "uv-seams.glb"
-    geo = mesh.read_glb(filename)
-    border_vertices = mesh.get_border_vertices(geo)
-
-    assert np.array_equiv(np.sort(border_vertices), [0, 2, 4, 6, 7, 9, 10])
+    local_mesh = mesh.read_glb(filename)
+    edges = mesh.get_edges_from_mesh(local_mesh)
+    border_vertices = mesh.get_border_vertices(edges)
+    assert np.array_equiv(
+        np.sort(border_vertices), [0, 2, 4, 6, 7, 9, 10]
+    ), "The vertices returned by get_border_vertices do not match the expected vertices."
