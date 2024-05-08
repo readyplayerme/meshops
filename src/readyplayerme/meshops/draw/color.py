fix(color): change method of attribute normalization
Now most extreme value, or 1, is used to scale relative to 0 as a fixed value.

src/readyplayerme/meshops/draw/color.py
@@ -124,33 +124,44 @@ def interpolate_values(start: Color, end: Color, num_steps: int) -> Color:
         return start[None, :] + t[:, None] * (end - start)
 
 
-def attribute_to_color(attribute: npt.NDArray[Any], *, normalize_per_channel: bool = True) -> Color:
+def attribute_to_color(attribute: npt.NDArray[Any], *, normalize_per_channel: bool = False) -> Color:
     """Convert an attribute to color values.
 
-    If necessary, normalize it and convert it to uint8.
+    If necessary, remap to [0, 255] range as uint8.
 
     :param attribute: The attribute to turn into color values.
-    :param normalize_per_channel: Whether to normalize each channel separately or across all channels.
+    :param normalize_per_channel: Whether to remap each channel separately or across all channels.
     Only if the attribute is not already uint8.
     :return: The attribute as uint8 color values.
     """
+    attribute = np.nan_to_num(attribute)
     if attribute.ndim == 0:
         msg = "Attribute has 0 dimensions. Must at least be a scalar (1 dimension)."
         raise ValueError(msg)
     if attribute.size > 1:  # Do not squeeze a scalar, as it will get 0 dimensions.
         attribute = np.squeeze(attribute)
     # If the attribute has 2 values, like UV coordinates, add a third value to make it RGB.
-    dim2 = 2
-    if attribute.ndim == dim2 and attribute.shape[1] == dim2:
+    dims_2d = 2
+    if attribute.ndim == dims_2d and attribute.shape[1] == dims_2d:
         attribute = np.pad(attribute, ((0, 0), (0, 1)), mode="constant", constant_values=0)
-    # A color should not have more than 4 channels, but we don't enforce it here.
+    # Now check if we're actually dealing with colors.
+    try:
+        _ = get_color_mode(attribute)
+    except ValueError:
+        raise
     # Normalize the attribute.
     if attribute.dtype != np.uint8:
         axis = 0 if normalize_per_channel else None
-        colors = np.nan_to_num(
-            (attribute - attribute.min(axis=axis, keepdims=True))
-            / (np.ptp(attribute, axis=axis, keepdims=True) + 1e-7)  # Small constant to avoid division by zero.
-        )
+        # If the attribute range is below 1, keep 1 as the maximum. Also avoids division by 0.
+        attribute_range = np.maximum(np.absolute(attribute).max(axis=axis, keepdims=True), 1)
+        colors = attribute / attribute_range
+        # If the minimum is less than 0, shift the array into the [0, 1] range.
+        column_mask = colors.min(axis=axis) < 0
+        if column_mask.any():
+            if axis is None:
+                colors = (colors + 1) * 0.5
+            elif axis == 0:
+                colors[:, column_mask] = (colors[:, column_mask] + 1) * 0.5
         colors = skimage.util.img_as_ubyte(colors)
     else:
         colors = attribute

tests/readyplayerme/meshops/unit/test_colors.py
@@ -210,49 +210,79 @@ def test_interpolate_values_should_fail(start_color, end_color, num_steps, expec
 @pytest.mark.parametrize(
     "attribute, per_channel_normalization, expected",
     [
-        # Test with single scalar attribute.
+        # single scalar attribute.
         (np.array([-5]), False, np.array([0], dtype=np.uint8)),
-        # Test with uint8 scalar attribute.
+        # uint8 scalar attribute.
         (
             np.array([128, 64, 192], dtype=np.uint8),
             False,
             np.array([128, 64, 192], dtype=np.uint8),
         ),
-        # Test with uint8 "1.5D" attribute.
+        # uint8 "1.5D" attribute.
         (
             np.array([[128], [64], [192]], dtype=np.uint8),
             False,
             np.array([128, 64, 192], dtype=np.uint8),
         ),
-        # Test with uint8 2D attribute.
+        # uint8 2D attribute.
         (
             np.array([[128, 192], [64, 32]], dtype=np.uint8),
             False,
             np.array([[128, 192, 0], [64, 32, 0]], dtype=np.uint8),
         ),
-        # Test with uint8 RGBA attribute.
+        # uint8 RGBA attribute.
         (
             np.array([[128, 64, 192, 32], [100, 150, 200, 50]], dtype=np.uint8),
             False,
             np.array([[128, 64, 192, 32], [100, 150, 200, 50]], dtype=np.uint8),
         ),
-        # Test with int32 RGB attribute and per_channel_normalization=True
+        # int32 RGB attribute and per_channel_normalization=True
         (
             np.array([[200, 0, 500], [-200, 0, 250], [100, 0, 0]], dtype=np.int32),
             True,
-            np.array([[255, 0, 255], [0, 0, 127], [191, 0, 0]], dtype=np.uint8),
+            np.array([[255, 0, 255], [0, 0, 128], [191, 0, 0]], dtype=np.uint8),
         ),
-        # Test with float32 attribute and per_channel_normalization=True
+        # int32 RGB attribute and per_channel_normalization=False
+        (
+            np.array([[200, 0, 500], [-200, 0, 250], [100, 0, 0]], dtype=np.int32),
+            False,
+            np.array([[178, 128, 255], [76, 128, 191], [153, 128, 128]], dtype=np.uint8),
+        ),
+        # float32 attribute and per_channel_normalization=True
         (
             np.array([[-2.0, 0.0, 0.0, 0.5], [0.0, 1.0, 0.0, 0.5], [0.0, 0.0, 1.0, 0.5]]),
             True,
-            np.array([[0, 0, 0, 0], [255, 255, 0, 0], [255, 0, 255, 0]], dtype=np.uint8),
+            np.array([[0, 0, 0, 128], [128, 255, 0, 128], [128, 0, 255, 128]], dtype=np.uint8),
         ),
-        # Test with float32 attribute and per_channel_normalization=False
+        # float32 attribute and per_channel_normalization=False
         (
             np.array([[-2.0, 0.0, 0.0, 0.5], [0.0, 1.0, 0.0, 0.5], [0.0, 0.0, 1.0, 0.5]]),
             False,
-            np.array([[0, 170, 170, 212], [170, 255, 170, 212], [170, 170, 255, 212]], dtype=np.uint8),
+            np.array([[0, 128, 128, 159], [128, 191, 128, 159], [128, 128, 191, 159]], dtype=np.uint8),
+        ),
+        # 3D with 1 channel -> squeezed to (2, 3)
+        (
+            np.array([[[1], [2]], [[3], [4]]], dtype=np.uint8),
+            False,
+            np.array([[1, 2, 0], [3, 4, 0]], dtype=np.uint8),
+        ),
+        # 1 NAN value
+        (
+            np.array([[np.nan, 0, 0], [0, 1, 0], [0, 0, 1]]),
+            True,
+            np.array([[0, 0, 0], [0, 255, 0], [0, 0, 255]], dtype=np.uint8),
+        ),
+        # All NAN values
+        (
+            np.array([[np.nan, np.nan, np.nan], [np.nan, np.nan, np.nan], [np.nan, np.nan, np.nan]]),
+            True,
+            np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]], dtype=np.uint8),
+        ),
+        # infinities
+        (
+            np.array([[np.inf, 0, 0], [0, -np.inf, 0], [0, 0, np.inf]]),
+            True,
+            np.array([[255, 128, 0], [0, 0, 0], [0, 128, 255]], dtype=np.uint8),
         ),
     ],
     ids=[
@@ -261,9 +291,14 @@ def test_interpolate_values_should_fail(start_color, end_color, num_steps, expec
         "uint8 1.5D",
         "uint8 2D",
         "uint8 RGBA",
-        "int32 RGB",
+        "int32 RGB, normalize per channel",
+        "int32 RGB, normalize globally",
         "float32 RGBA per channel",
         "float32 RGBA global",
+        "3D with 1 channel",
+        "1 NAN value",
+        "all NAN values",
+        "infinities",
     ],
 )
 def test_attribute_to_color(attribute, per_channel_normalization, expected):
@@ -277,12 +312,16 @@ def test_attribute_to_color(attribute, per_channel_normalization, expected):
 @pytest.mark.parametrize(
     "attribute",
     [
-        # Test with empty attribute
+        # Empty attribute
         np.array([]),
-        # Test with 0-dimensional attribute
+        # 0-dimensional attribute
         np.array(5),
+        # 2D with 5 channels
+        np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]),
+        # 3D with 2 channels
+        np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]]),
     ],
-    ids=["empty", "0-dimensional attribute"],
+    ids=["empty", "0-dimensional attribute", "2D with 5 channels", "3D with 2 channels"],
 )
 def test_attribute_to_color_should_fail(attribute):
     """Test the attribute_to_color function with invalid inputs."""